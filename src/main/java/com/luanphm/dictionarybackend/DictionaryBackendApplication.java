9a4de164cb63b0304eda3f29b799609988277c8d
phanhoangminhluan <phanhoangminhluan@gmail.com>
2020-01-20 23:45:12 +0700

    init project backend

---
+++ src/main/java/com/luanphm/dictionarybackend/DictionaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.luanphm.dictionarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DictionaryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luanphm/dictionarybackend/DictionaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luanphm.dictionarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
