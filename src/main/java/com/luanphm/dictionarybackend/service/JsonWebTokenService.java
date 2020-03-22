d1088f5529fa49028c45296ba23b9589b04fc7c6
Phan Hoàng Minh Luân <phanhoangminhluan@gmail.com>
2020-03-23 03:00:04 +0700

    implement logout feature with Redis Database

---
+++ src/main/java/com/luanphm/dictionarybackend/service/JsonWebTokenService.java
@@ -0,0 +1,11 @@
+package com.luanphm.dictionarybackend.service;
+
+public interface JsonWebTokenService {
+
+    void add(String username, String token);
+
+    boolean isExisted(String key, String token);
+
+    boolean deleteById(String key);
+
+}
---
+++ src/main/java/com/luanphm/dictionarybackend/service/JsonWebTokenServiceImpl.java
@@ -0,0 +1,56 @@
+package com.luanphm.dictionarybackend.service;
+
+import com.luanphm.dictionarybackend.configuration.security.JwtAuthenticationFilter;
+import com.luanphm.dictionarybackend.constant.SecurityUtils;
+import com.luanphm.dictionarybackend.repository.json_web_token.JsonWebTokenRepository;
+import com.luanphm.dictionarybackend.utility.CommonUtilities;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class JsonWebTokenServiceImpl implements JsonWebTokenService {
+
+    @Autowired
+    public RedisTemplate<String, String> redisTemplate;
+
+    @Autowired
+    JsonWebTokenRepository jsonWebTokenRepository;
+
+    @Override
+    public void add(String username, String token) {
+        String currentDate = CommonUtilities.getCurrentDateTimeForRedis();
+        redisTemplate.opsForValue().set(username + " - " + currentDate, token);
+        redisTemplate.expire(username, JwtAuthenticationFilter.EXPIRATION_TIME, TimeUnit.MILLISECONDS);
+    }
+    @Override
+    public boolean isExisted(String username, String token) {
+        if (username == null || username.isEmpty()) return false;
+        String key = findById(username, token);
+        return key != null;
+    }
+
+    @Override
+    public boolean deleteById(String token) {
+        if (token == null || token.isEmpty()) return false;
+        String username = SecurityUtils.getCurrentUser();
+        String key = findById(username, token);
+        return redisTemplate.delete(key);
+    }
+
+    private String findById(String username, String token) {
+        Set<String> keys = redisTemplate.keys(username + "*");
+        String tk = token.replace("Bearer ", "");
+        String resultKey = null;
+        for (String key : keys) {
+            String redisToken = redisTemplate.opsForValue().get(key);
+            if (redisToken.equals(tk)) {
+                resultKey = key;
+            }
+        }
+        return resultKey;
+    }
+}
